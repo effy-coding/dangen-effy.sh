cad1d4649587ddbe4578208f6552cb129f8617e1
dangen <dangen.effy@gmail.com>
2022-12-19 15:28:41 +0900

    Update dangen-effy.sh

--- dangen-effy.sh
+++ dangen-effy.sh
@@ -64,7 +64,8 @@ export EDITOR=/usr/local/bin/nvim
 EOF
 
 # Install vim-plug
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 #####################
 # PL
